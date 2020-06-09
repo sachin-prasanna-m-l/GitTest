62aade698eb4d9c555f46c876e729f7b1a0e6a0f
sachin-prasanna-m-l <sachinprasanna.ml2017csec@sece.ac.in>
2020-06-09 01:36:17 +0000

    location of jar done

--- JavaDataBase/src/Main.java
+++ JavaDataBase/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		LoginEntity longinEntity = new LoginEntity(sc.next(),sc.next());
+        LoginEntity longinEntity = new LoginEntity(sc.next(),sc.next());
+        sc.close();
 	}
 
 }
