42c3ed5bbbaca4b5e99fe372e01f512f7dce8388
sachin-prasanna-m-l <sachinprasanna.ml2017csec@sece.ac.in>
2020-06-09 02:22:20 +0000

    main file updated

--- JavaDataBase/src/Main.java
+++ JavaDataBase/src/Main.java
@@ -4,6 +4,7 @@ import com.login.entity.LoginEntity;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+        //main
 		Scanner sc = new Scanner(System.in);
         LoginEntity longinEntity = new LoginEntity(sc.next(),sc.next());
         sc.close();
